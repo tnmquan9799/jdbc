a6b4c9219e0cf08fb578323b143df8c47550de9c
tnmquan9799 <tnmquan9799@gmail.com>
2019-03-28 10:08:37 +0700

    CRUD UPDATE

--- src/vn/edu/vnuk/jdbc/test/TestUpdateContact.java
+++ src/vn/edu/vnuk/jdbc/test/TestUpdateContact.java
@@ -12,6 +12,7 @@ public class TestUpdateContact {
 		ContactDao contactDao = new ContactDao();
 		
 		long id;
+		String name, email, address;
 	    Scanner sc = new Scanner(System.in);
 	    
 	    System.out.println("Input id you want to update: ");
@@ -19,10 +20,40 @@ public class TestUpdateContact {
 		
 		Contact newContact = contactDao.read(id);
 		
-		newContact.setName("UpdatedName");
-		newContact.setEmail("UpdatedEmail@Asd");
-//		newContact.setAddress("asdasd");
+		System.out.println("INPUT NEW DATA! IF NOT CHANGE, ENTER '0' ");
 		
+		System.out.println("Input new name: ");
+		name = sc.next();
+		
+		System.out.println("Input new email: ");
+		email = sc.next();
+		
+		System.out.println("Input new address: ");
+		address = sc.next();
+		
+		if (!name.equals("0")) {
+			newContact.setName(name);
+			System.out.println("NAME CHANGED!");
+		} else {
+			System.out.println("NAME UNCHANGED!");
+		}
+		
+		if (!email.equals("0")) {
+			newContact.setEmail(email);
+			System.out.println("EMAIL CHANGED");
+		} else {
+			System.out.println("EMAIL UNCHANGED");
+
+		}
+		
+		if (!address.equals("0")) {
+			newContact.setAddress(address);
+			System.out.println("ADDRESS CHANGED");
+		} else {
+			System.out.println("ADDRESS UNCHANGED");
+
+		}
+
 		contactDao.update(id, newContact);
 		
 	}
